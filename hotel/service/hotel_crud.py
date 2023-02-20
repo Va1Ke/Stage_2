dc5e6c9518c113916bb6b159c38db2b9027190c2
Va1Ke <vladpc2018@gmail.com>
2023-02-20 14:49:21 +0200

    fixed troubles

--- hotel/service/hotel_crud.py
+++ hotel/service/hotel_crud.py
@@ -1,13 +1,11 @@
-# from hotel.service.schemas import hotel_schemas
-# from hotel.models.models import hotel
-#
-# class Hotel_crud:
-#     def __init__(self, db):
-#         self.db = db
-#
-#     async def add_room(self, room: hotel_schemas.AddRoom) -> hotel_schemas.RoomInfoReturn:
-#         db_room = hotel.insert().values(area=room.area, number_of_beds=room.number_of_beds,
-#                                         price_for_a_night=room.price_for_a_night)
-#         record_id = await self.db.execute(db_room)
-#         return hotel_schemas.RoomInfoReturn(**room.dict(), id=record_id,)
-#
+from service.schemas import hotel_schemas
+from models.models import Hotel
+
+class Hotel_crud:
+
+    async def add_room(self, room: hotel_schemas.AddRoom) -> hotel_schemas.RoomInfoReturn:
+        db_room = Hotel.insert().values(area=room.area, number_of_beds=room.number_of_beds,
+                                        price_for_a_night=room.price_for_a_night)
+        record_id = await self.db.execute(db_room)
+        return hotel_schemas.RoomInfoReturn(**room.dict(), id=record_id,)
+
